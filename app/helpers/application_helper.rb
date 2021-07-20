0f4ce8a21a7c7b3783292738649c0aae3432ed3d
Horisuke96 <horisuke_52m@yahoo.co.jp>
2021-07-20 00:23:03 +0000

    [add]flash_and_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
 
+  def full_title(title = "")
+    base_title = "家計boo"
+      if admin_signed_in?
+      base_title + "|" + "(管理者) #{title}"
+      else
+      base_title + "|" + "#{title}"
+      end
+  end
+
   def full_name(user_id)
     user = User.find_by(id: user_id)
     user.last_name + user.first_name
