c0199facf02d3164cd25eab084888314390164ea
Horisuke96 <horisuke_52m@yahoo.co.jp>
2021-07-26 01:54:16 +0000

    [add]quit

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :expenses, dependent: :destroy
 
+  def active_for_authentication?
+    super && (self.is_deleted == false)
+  end
 end
