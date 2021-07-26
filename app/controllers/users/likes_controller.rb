4acbdfdeaae06dfbe9036ff37a248339c3feaf28
Horisuke96 <horisuke_52m@yahoo.co.jp>
2021-07-26 23:39:28 +0000

    [fix]いいね機能

--- app/controllers/users/likes_controller.rb
+++ app/controllers/users/likes_controller.rb
@@ -4,14 +4,12 @@ class Users::LikesController < ApplicationController
     @post = Post.find(params[:post_id])
     @like = current_user.likes.new(post_id: @post.id)
     @like.save
-    redirect_to posts_path
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @like = current_user.likes.find_by(post_id: @post.id)
     @like.destroy
-    redirect_to posts_path
   end
 
 end
